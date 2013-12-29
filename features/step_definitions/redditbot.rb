aa1b8993fc1413cf3248c561a4a6bbd9ce376f03
Luis <mikeylopez@gmail.com>
2013-12-29 03:43:53 -0500

    login steps

--- features/step_definitions/redditbot.rb
+++ features/step_definitions/redditbot.rb
@@ -1,4 +1,27 @@
 Given(/^I load reddit\.com in a browser$/) do
- $reddit.goto 
+ $redditbot.goto_site
 end
 
+When(/^I enter "(.*?)" as username$/) do |username|
+ $redditbot.enter_username(username)
+end
+
+When(/^I enter "(.*?)" as password$/) do |password|
+ $redditbot.enter_password(password)
+end
+
+When(/^I hit login$/) do
+ $redditbot.submit_login 
+end
+
+Then(/^I am logged in$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^debug mode$/) do
+# binding.pry
+end
+
+When(/^wait "(.*?)" secs$/) do |secs|
+ sleep(secs.to_i)
+end

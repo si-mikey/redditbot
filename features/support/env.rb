eeb44c02859895eb6a1f379032595c56d3ddad2c
Luis <mikeylopez@gmail.com>
2013-12-31 17:18:48 -0500

    set rspec config for using expectations

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,6 @@
 require 'selenium-webdriver'
 require 'cucumber'
+require 'rspec'
 require 'pry'
 require_relative 'lib/Objects'
 require_relative 'lib/Actions'
@@ -7,6 +8,12 @@ require_relative 'lib/Actions'
 $browser = Selenium::WebDriver::for :chrome
 @objects = Reddit::Page::Objects.new($browser)
 $redditbot = Reddit::Bot::Actions.new(@objects)
+
+RSpec.configure do |config|
+ config.expect_with :rspec do |c|
+  c.syntax = :expect
+ end 
+end
 
 at_exit do
  $browser.quit

222bd0f6a21bf790d6b36246574de7f46ed56ebc
Luis <mikeylopez@gmail.com>
2014-01-15 22:33:55 -0500

    objects module

--- features/support/lib/Actions.rb
+++ features/support/lib/Actions.rb
@@ -3,33 +3,30 @@ module Reddit
   
   class Actions
 
-   def initialize(objects)
-    @objects = objects
-   end
-
+		Objects = Reddit::Page::Objects		
+		
     def goto_site
-     $browser.navigate.to ENV['MAIN_URL']
+      $browser.navigate.to Objects::SITE_URL 
     end
 
     def enter_username(username)
-     @objects.username.send_keys(username)
+      Objects.username.send_keys(username)
     end
 
     def enter_password(password)
-     @objects.password.send_keys(password)
+      Objects.password.send_keys(password)
     end
 
     def submit_login
-     @objects.login_btn.click
+      Objects.login_btn.click
     end     
 
     def logged_in?
-      @objects.wait.until {!!(@objects.user_link.text.match $bot['name'])}
+      Objects.wait.until {!!(Objects.user_link.text.match $bot['name'])}
     end 
 
-    def stories
-      @objects.stories
-    	
+    def upvote_story 
+      Objects.stories
     end
 
 
